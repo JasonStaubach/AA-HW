b5a6e035161a72308dfac49318ecfcfa80da7179
JasonStaubach <jbstaubach@gmail.com>
2023-05-21 22:44:16 -0400

    added first attempt at bfs sorting

--- W4D1/graphnode.rb
+++ W4D1/graphnode.rb
@@ -2,10 +2,23 @@ class GraphNode
 
     attr_accessor :neighbors
     attr_reader :value
-    
+
     def initialize(value)
         @value = value
         @neighbors = []
+    end
+
+    def bfs(starting_node, target_value)
+        visited = Set.new()
+        queue = starting_node 
+        until queue.empty?
+            checker = queue.shift
+            return checker if starting_node.value == target_value
+            visited.add(checker)
+            checker.neighbors.each {|ele| queue << ele unless visited.include?(ele)}
+        end
+        return false
+
     end
 
 end
@@ -16,3 +29,8 @@ c = GraphNode.new('c')
 d = GraphNode.new('d')
 e = GraphNode.new('e')
 f = GraphNode.new('f')
+
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
