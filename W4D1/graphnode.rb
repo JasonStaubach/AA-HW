f042021ad51614703300d3e13af822a85f6f1d2b
JasonStaubach <jbstaubach@gmail.com>
2023-05-21 22:36:40 -0400

    added initialize and some test data

---
+++ W4D1/graphnode.rb
@@ -0,0 +1,14 @@
+class GraphNode
+    def initialize(value)
+        @value = value
+        @neighbors = []
+    end
+
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
