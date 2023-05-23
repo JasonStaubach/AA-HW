7da6f9451054f701798bf0cc954c3c472c60cb62
JasonStaubach <jbstaubach@gmail.com>
2023-05-22 21:29:58 -0400

    figuring out rules finished initialize, place stones, and valid moves

--- W4D2/practice-for-ch-ruby-mancala-main/lib/board.rb
+++ W4D2/practice-for-ch-ruby-mancala-main/lib/board.rb
@@ -1,19 +1,41 @@
+require "byebug"
+
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
     @cups = Array.new(14){[]}
-    @non_store = 4
+    # debugger
+    self.place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    (0..5).each do |i|
+      @cups[i] += [:stone, :stone, :stone, :stone]
+    end
+    @cups
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 14
+    raise "Starting cup is empty" if @cups[start_pos].empty?
+    true
   end
 
   def make_move(start_pos, current_player_name)
+    num_stones = @cups[start_pos].length
+    dump = []
+    until @cups[start_pos].empty?
+      dump << @cups[start_pos].shift
+    end
+
+    i = start_pos + 1
+    until dump.empty?
+      i == i % 14
+      @cups[i] << dump.shift
+      i += 1
+    end
   end
 
   def next_turn(ending_cup_idx)
