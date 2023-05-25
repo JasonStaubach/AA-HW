198652c14db28dbf5e7166729e4a83e49dd0d0d2
JasonStaubach <jbstaubach@gmail.com>
2023-05-25 05:53:59 -0400

    compared big O sorting and got every octopus method but fast dance

---
+++ W4D4/octopus.rb
@@ -0,0 +1,63 @@
+FISH = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 
+ 'fiiiissshhhhhh']
+ tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", 
+               "left",  "left-up"]
+public 
+
+ def sluggish_octopus
+    longest_fish = ""
+    FISH.each do |fish1|
+        FISH.each do |fish2|
+            if fish1.length > fish2.length && fish1.length > longest_fish.length
+                longest_fish = fish1
+            end
+        end
+    end
+    longest_fish
+end
+
+def dominant_octopus
+    return self.merge_sort.last
+end
+
+def merge_sort
+    return self if self.length < 2
+    mid = self.length / 2
+    
+    left = self[0...mid]
+    right = self[mid..-1]
+    return merge_length(left.merge_sort,right.merge_sort)
+end
+
+def merge_length(left, right)
+    holder = []
+    until left.empty? || right.empty?
+        case left[0].length <=> right[0].length
+        when -1
+            holder << left.shift
+        when 0
+            holder << right.shift
+        when 1
+            holder << right.shift
+        end
+    end
+    holder + left + right
+end
+
+def clever_octopus
+    fish = self[0]
+    (1...self.length).each {|i| fish = self[i] if self[i].length > fish.length}
+    fish
+end
+
+def slow_dance(dir, moves_arr = [])
+    moves_arr.each_with_index {|move, i| return i if move == dir}
+    return "move not found"
+end
+
+
+p sluggish_octopus
+p FISH.dominant_octopus
+p FISH.clever_octopus
+p slow_dance("up", tiles_array)
+p slow_dance("right-down", tiles_array)
