ffa31b22606868730b9756f30b21c55208dd8bb8
JasonStaubach <jbstaubach@gmail.com>
2023-05-25 06:00:24 -0400

    added fast_dance, but fast_dance will break if given a move not in hash

--- W4D4/octopus.rb
+++ W4D4/octopus.rb
@@ -2,6 +2,8 @@ FISH = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh',
  'fiiiissshhhhhh']
  tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", 
                "left",  "left-up"]
+tiles_hash = {"up" => 0, "right-up" => 1, "right" => 2, "right-down" => 3, "down" => 4, "left-down" => 5, 
+"left" => 6,  "left-up" => 7 }
 public 
 
  def sluggish_octopus
@@ -55,9 +57,15 @@ def slow_dance(dir, moves_arr = [])
     return "move not found"
 end
 
+def fast_dance(dir, move_hash = {})
+    move_hash[dir]
+end
+
 
 p sluggish_octopus
 p FISH.dominant_octopus
 p FISH.clever_octopus
 p slow_dance("up", tiles_array)
 p slow_dance("right-down", tiles_array)
+p fast_dance("up", tiles_hash)
+p fast_dance("right-down", tiles_hash)
