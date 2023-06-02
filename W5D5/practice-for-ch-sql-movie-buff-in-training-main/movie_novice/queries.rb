10b664720befd32830c9409c73cdc7cbc58c1975
JasonStaubach <jbstaubach@gmail.com>
2023-06-02 12:19:05 -0400

    fixing some of movie buff

--- W5D5/practice-for-ch-sql-movie-buff-in-training-main/movie_novice/queries.rb
+++ W5D5/practice-for-ch-sql-movie-buff-in-training-main/movie_novice/queries.rb
@@ -30,19 +30,19 @@
 
 def find_angelina
   # find Angelina Jolie by name in the actors table
-
+  Actor.find_by(name: "Angelina Jolie")
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-  
+  Movie.select(:title).where("score >= 9")
 end
 
 def star_wars
   # display the id, title, and year of each Star Wars movie in movies
   # hint: use 'select' and 'where'
-  
+  Movie.select(:id,:title,:yr).where("title LIKE '%Star Wars'")
 end
 
 
@@ -50,7 +50,7 @@ def below_average_years
   # display each year with movies scoring under 5, with the count of movies
   # scoring under 5 aliased as bad_movies, in descending order
   # hint: use 'select', 'where', 'group', 'order'
-  
+  Movie.select(:yr, "COUNT(*) AS bad_movies").where("score < 5").group(:yr).order("bad_movies DESC")
 end
 
 def alphabetized_actors
