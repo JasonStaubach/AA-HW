e55c429f89a0f4156053d86ef3575c2a912c5a26
JasonStaubach <jbstaubach@gmail.com>
2023-06-21 10:20:51 -0400

    did phase 3, feels wrong tho, im not calling elephant class

--- W8D3/practice-for-ch-js-intro-js-problems2-main/phase2.js
+++ W8D3/practice-for-ch-js-intro-js-problems2-main/phase2.js
@@ -20,15 +20,37 @@ function Elephant(name, height, tricks){
         randomT = (Math.floor(Math.random() * this.tricks.length));
         console.log(this.tricks[randomT]);
     }
+    // Elephant.prototype.paradeHelper = function(elephant){
+    //     console.log(`${elephant.name} is trotting by!`)
+    // }
+    
 }
-let larry = new Elephant('Larry', 60, ["Stomping around"]);
-console.log(larry.height);
-console.log(larry.trumpet());
-console.log(Elephant.prototype);
-larry.grow();
-console.log(larry.height);
-larry.addTrick("Shake trunk");
-console.log(larry.tricks);
-console.log(larry.play())
-console.log(larry.play())
-console.log(larry.play())
+
+function paradeHelper(elephant){
+    console.log(`${elephant.name} is trotting by!`)
+}
+
+function parade(herd, callback){
+    herd.forEach( el => { callback(el) } );
+}
+
+
+let larry = new Elephant('Larry', 60, ["stomping around", "shaking trunk", "shenanigans"]);
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [larry, ellie, charlie, kate, micah];
+
+parade(herd, paradeHelper);
+// console.log(larry.height);
+// console.log(larry.trumpet());
+// console.log(Elephant.prototype);
+// larry.grow();
+// console.log(larry.height);
+// larry.addTrick("Shake trunk");
+// console.log(larry.tricks);
+// console.log(larry.play())
+// console.log(larry.play())
+// console.log(larry.play())
