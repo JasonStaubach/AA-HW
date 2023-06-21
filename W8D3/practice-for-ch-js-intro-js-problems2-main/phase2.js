16ad66b4b4615132a40cef1137ee6b7b720b9647
JasonStaubach <jbstaubach@gmail.com>
2023-06-21 09:48:11 -0400

    fixed trumpet and added grow as prototype functions

--- W8D3/practice-for-ch-js-intro-js-problems2-main/phase2.js
+++ W8D3/practice-for-ch-js-intro-js-problems2-main/phase2.js
@@ -2,11 +2,18 @@ function Elephant(name, height){
     this.name = name;
     this.height = height;
 
-    this.trumpet = function () {
+    
+    Elephant.prototype.trumpet = function () {
         console.log(`${name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
-    }
-}
+    };
 
-let larry = new Elephant('Larry', 60)
-console.log(larry.height)
-console.log(larry.trumpet())
+    Elephant.prototype.grow = function () {
+        this.height += 12;
+    };
+}
+let larry = new Elephant('Larry', 60);
+console.log(larry.height);
+console.log(larry.trumpet());
+console.log(Elephant.prototype);
+larry.grow();
+console.log(larry.height);
